90269e51f771dd904874fcbb5cc90f137eb8e8c1
thiger7 <adf0423@gmail.com>
2024-02-27 09:25:14 +0900

    chore: linksを表示するところまで

--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+//HackerNewsの１つ１つの投稿
+let links = [
+  {
+    id: "link-0",
+    description: "GraphQLチュートリアルをUdemyで学ぶ",
+    url: "www.udemy-graphql-tutorial.com"
+  }
+]
+
+
 //GraphQLスキーマの定義
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url:  String!
   }
 `;
 
 //リゾルバ関数
 const resolvers = {
   Query: {
-    info: () => "HackerNewsクローン"
+    info: () => "HackerNewsクローン",
+    feed: () => links,
   },
 };
 
