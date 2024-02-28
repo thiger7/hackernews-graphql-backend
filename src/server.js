da72a1dd0b93f44a24226b7cc2751f0a9394d847
thiger7 <adf0423@gmail.com>
2024-02-28 09:14:23 +0900

    サーバー側のデータベース

--- src/server.js
+++ src/server.js
@@ -2,41 +2,38 @@ const { ApolloServer, gql } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
-//HackerNewsの１つ１つの投稿
-let links = [
-  {
-    id: "link-0",
-    description: "GraphQLチュートリアルをUdemyで学ぶ",
-    url: "www.udemy-graphql-tutorial.com"
-  }
-]
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient();
 
 //リゾルバ関数
 const resolvers = {
   Query: {
     info: () => "HackerNewsクローン",
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    }
   },
 
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id:  `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-
-      links.push(link);
-      return link;
-    }
-  }
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args,description,
+        },
+      });
+      return newLink;
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
+  context: {
+    prisma
+  }
 });
 
 server.listen().then(({ url }) => console.log(`${url}でサーバーを起動中・・・`));
